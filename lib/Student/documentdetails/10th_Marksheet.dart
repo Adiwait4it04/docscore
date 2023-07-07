81daa6b323cbf72782e467b353f397e8078f5a4e
Aditya Saha <adityasaha04@gmail.com>
2023-07-07 16:15:36 +0530

    ✨

--- lib/Student/documentdetails/10th_Marksheet.dart
+++ lib/Student/documentdetails/10th_Marksheet.dart
@@ -1,9 +1,10 @@
 import 'package:docscore/Student/add_docs.dart';
-import 'package:docscore/Student/add_docs.dart';
 import 'package:docscore/Student/student_home.dart';
 import 'package:docscore/resources/constants/colors.dart';
+import 'package:docscore/widgets/test_form_field.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:dotted_border/dotted_border.dart';
 import '../../resources/constants.dart';
 
 class adddocs0 extends StatefulWidget {
@@ -12,6 +13,8 @@ class adddocs0 extends StatefulWidget {
   @override
   State<adddocs0> createState() => _adddocs0State();
 }
+
+final TextEditingController _percent_calc = TextEditingController();
 
 List<String> _sections = [
   "CBSE",
@@ -25,97 +28,174 @@ class _adddocs0State extends State<adddocs0> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          gradient: backgroundGradient(),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Center(
-              child: Text('10th Marksheet'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: DropdownButtonFormField(
-                validator: (value) {
-                  if (value == null) {
-                    return "Select your section";
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context)),
-                  errorBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context)),
-                  contentPadding: EdgeInsets.all(8.0),
-                  filled: true,
-                  fillColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: backgroundGradient(),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, left: 15),
+                      child: IconButton(
+                        onPressed: () {
+                          replaceScreen(
+                            context,
+                            adddocs(),
+                          );
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: ButtonColor(),
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, left: 15),
+                      child: Text(
+                        "10th Marksheet",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 35,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                hint: const Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+                  child: DropdownButtonFormField(
+                    validator: (value) {
+                      if (value == null) {
+                        return "Select your section";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: Divider.createBorderSide(context),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: Divider.createBorderSide(context),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: Divider.createBorderSide(context),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: Divider.createBorderSide(context),
+                      ),
+                      contentPadding: const EdgeInsets.all(8.0),
+                      filled: true,
+                    ),
+                    hint: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      child: Text("Select your Board"),
+                    ),
+                    items: _sections.map((String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        boards = value!;
+                      });
+                    },
                   ),
-                  child: Text("Select your Board"),
                 ),
-                items: _sections.map((String item) {
-                  return DropdownMenuItem(
-                    value: item,
-                    child: Text(item),
-                  );
-                }).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    boards = value!;
-                  });
-                },
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(
-                  () {
-                    additems(
-                      name[0],
-                    );
-                    buttonStates[0] = false;
-                  },
-                );
-                replaceScreen(
-                  context,
-                  const Student_home_page(regno: "RA2111051010009"),
-                );
-              },
-              child: InkWell(
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  decoration: const BoxDecoration(
-                    color: Color(0xFF090F30),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  child: TextForm(
+                    textEditingController: _percent_calc,
+                    hintText: '% of English + Maths + Science+2 subjects',
+                    textInputType: TextInputType.number,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter a percentage";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: DottedBorder(
+                    color: Colors.black,
+                    strokeWidth: 4,
+                    dashPattern: const [30, 30],
+                    child: Container(
+                      height: 250,
+                      width: 320,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: const Center(
+                        child: IconButton(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.folder_shared,
+                            size: 50,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  child: const Text(
-                    "Upload Document",
-                    style: TextStyle(
-                      color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, bottom: 40),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(
+                        () {
+                          additems(
+                            name[0],
+                          );
+                          buttonStates[0] = false;
+                        },
+                      );
+                      replaceScreen(
+                        context,
+                        const Student_home_page(regno: "RA2111051010009"),
+                      );
+                    },
+                    child: InkWell(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF090F30),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        child: const Text(
+                          "Upload Document",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
