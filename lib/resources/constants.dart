799e92155732740552d27d18d89f4253edf2edf9
Divyansh Chauhan <106025754+Quarkblue@users.noreply.github.com>
2023-07-23 22:21:43 +0530

    minor change

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -1,11 +1,16 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:docscore/models/users.dart' as user_model;
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
 List<String> items = [];
 int i = 0;
 int j = i++;
+final regno = user_model.User().getStudentRegNoFromUid(_auth.currentUser!.uid);
 
 nextScreen(BuildContext context, Widget screen) {
   return Navigator.push(
@@ -30,30 +35,32 @@ StudentHomeWidget(index) {
   return Padding(
     padding:
         const EdgeInsets.only(top: 30.0, left: 110, right: 110, bottom: 30),
-    child: Container(
-      height: 100,
-      decoration: BoxDecoration(
-        color: const Color(0xFFE9EFFF),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Icon(
-              Icons.document_scanner,
-              color: Color(0xFF2A519D),
-            ),
-            FittedBox(
-              child: Text(
-                items[index],
-                style: GoogleFonts.montserrat(
-                  color: const Color(0xFF2A519D),
-                  fontWeight: FontWeight.w800,
+    child: InkWell(
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: const Color(0xFFE9EFFF),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Icon(
+                Icons.document_scanner,
+                color: Color(0xFF2A519D),
+              ),
+              FittedBox(
+                child: Text(
+                  items[index],
+                  style: GoogleFonts.montserrat(
+                    color: const Color(0xFF2A519D),
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     ),
