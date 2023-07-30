c4185250d5ac7a1afd38e0fc1c35ff00aa392736
Aditya Saha <adityasaha04@gmail.com>
2023-07-31 02:06:42 +0530

    ✨ orientation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:docscore/resources/constants/colors.dart';
 import 'package:docscore/resources/constants.dart';
 import 'package:docscore/Student/student_signup.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +29,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       title: 'Docscore',
       theme: ThemeData(
